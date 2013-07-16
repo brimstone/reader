4c94b7941a20e5f301c94e870693fa684a7e2b51
Matt <brimstone@the.narro.ws>
2013-07-16 13:43:57 -0400

    * Added a controller for the items * Added a service to allow the Feeds controller to update the Items controller

--- js/app.js
+++ js/app.js
@@ -1,10 +1,33 @@
-angular.module( 'ReaderApp', [ 'ngResource' ] )
+var Reader_App = angular.module( 'Reader_App', [ 'ngResource' ] );
 
+Reader_App.factory("Feed_Service", function($rootScope) {
+		var Feed_Service = {};
+		Feed_Service.current_feed = 0;
+		Feed_Service.update_feed = function(feed) {
+			this.current_feed = feed;
+			$rootScope.$broadcast('updated_feed');
+		}
+		return Feed_Service;
+});
 
-angular.module('ReaderApp').controller('FeedCtlr', function($scope, $resource) {
+function Feed_Ctlr($scope, $resource, Feed_Service) {
 	var Feeds = $resource('/api/feeds/:action');
 	$scope.feeds = Feeds.query({}, function(data) {
 	}, function(err){
 		console.log(err);
 	});
-});
+	$scope.update_feed = function(feed) {
+		Feed_Service.update_feed(feed);
+	};
+};
+
+function Item_Ctlr($scope, $resource, Feed_Service) {
+	$scope.items = [];
+	$scope.$on('updated_feed', function(){
+		console.log("We should go grab items based on", Feed_Service.current_feed);
+	});
+}
+
+// I may not need this
+// Feed_Ctlr.$inject = ['$scope', 'Feed_Service'];
+
