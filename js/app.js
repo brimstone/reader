905c68a5e72bb9451723ec492244535d70679693
Matt <brimstone@the.narro.ws>
2013-07-19 16:36:27 -0400

    * Made list work

--- js/app.js
+++ js/app.js
@@ -1,39 +1,44 @@
 var Reader_App = angular.module( 'Reader_App', [ 'ngResource' ] );
 
-Reader_App.factory("Feed_Service", function($rootScope) {
-		var Feed_Service = {};
-		Feed_Service.current_feed = 0;
-		Feed_Service.update_feed = function(feed) {
-			this.current_feed = feed;
-			$rootScope.$broadcast('updated_feed');
-		}
-		return Feed_Service;
+Reader_App.factory("Feed_Service", function($rootScope, $resource) {
+		return {current: {feed: 0, article: 0},
+			// I wonder if there's a more... angular way of doing this setter
+			select: function(key, value) {
+				this.current[key] = value;
+				$rootScope.$broadcast('updated_' + key);
+			},
+			Feeds: $resource('/api/feeds/:action'),
+			Items: $resource('/api/items/:action'),
+		};
 });
 
-function Feed_Ctlr($scope, $resource, Feed_Service) {
-	var Feeds = $resource('/api/feeds/:action');
-	$scope.feeds = Feeds.query({}, function(data) {
+function Feed_Ctlr($scope, Feed_Service) {
+	$scope.feeds = Feed_Service.Feeds.query({}, function(data) {
 	}, function(err){
 		console.log(err);
 	});
-	$scope.update_feed = function(feed) {
-		Feed_Service.update_feed(feed);
+	$scope.select = function(feed) {
+		Feed_Service.select('feed', feed);
 	};
 };
 
-function Item_Ctlr($scope, $resource, Feed_Service) {
-	var Items = $resource('/api/items/:action');
-	$scope.items = Items.query({feed: 1});
+function List_Ctlr($scope, Feed_Service) {
+	$scope.articles = Feed_Service.Items.query({feed: 1});
 	$scope.$on('updated_feed', function(){
-		feed = Feed_Service.current_feed;
-		$scope.items = Items.query({feed: feed.id}, function(data){
-			console.log($scope.items);
+		feed = Feed_Service.current.feed;
+		$scope.articles = Feed_Service.Items.query({feed: feed.id}, function(data){
+			console.log($scope.articles);
 		}, function(err) {
 			console.log(err);
 		});
 	});
+	$scope.select = function(article) {
+		Feed_Service.select('article', article);
+	};
 }
 
-// I may not need this
-// Feed_Ctlr.$inject = ['$scope', 'Feed_Service'];
-
+function Article_Ctlr($scope, Feed_Service) {
+	$scope.$on('updated_article', function(){
+		$scope.article = Feed_Service.current.article;
+	});
+}
