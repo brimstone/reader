bc1cccdc8a429d406735c3c8f11b58f28a74bac5
Matt <brimstone@the.narro.ws>
2013-07-20 21:27:33 -0400

    * disabled buttons for now * Started key detection code

--- js/app.js
+++ js/app.js
@@ -1,15 +1,23 @@
 var Reader_App = angular.module( 'Reader_App', [ 'ngResource' ] );
 
 Reader_App.factory("Feed_Service", function($rootScope, $resource) {
-		return {current: {feed: 0, article: 0},
-			// I wonder if there's a more... angular way of doing this setter
-			select: function(key, value) {
-				this.current[key] = value;
-				$rootScope.$broadcast('updated_' + key);
-			},
-			Feeds: $resource('/api/feeds/:action'),
-			Items: $resource('/api/items/:action'),
-		};
+	document.onkeyup = function(e) {
+		var key = (window.event) ? event.keyCode : e.keyCode;
+		//if (e.shiftKey && 65 <= key && key <= 90)
+		//	key =+ 32;
+		console.log(key);
+	}
+	// build our service object and return it
+	return {
+		current: {feed: 0, article: 0},
+		// I wonder if there's a more... angular way of doing this setter
+		select: function(key, value) {
+			this.current[key] = value;
+			$rootScope.$broadcast('updated_' + key);
+		},
+		Feeds: $resource('/api/feeds/:action'),
+		Items: $resource('/api/items/:action'),
+	};
 });
 
 function Feed_Ctlr($scope, Feed_Service) {
@@ -42,3 +50,4 @@ function Article_Ctlr($scope, Feed_Service) {
 		$scope.article = Feed_Service.current.article;
 	});
 }
+
